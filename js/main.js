fdf62ce3c00bcb1790c0736e3091fa0c3fa3049b
Joseph Graves <jcg97@Josephs-MacBook-Pro.local>
2022-05-21 10:18:36 -0400

    Added 3 new buttons for the colors

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+document.querySelector("button").addEventListener("color", setNewColor);
+
 const randomBetween = (min, max) => {
     min + Math.floor(Math.random() * (max - min + 1));
 }
 
-const rgb = "rgb(" + randomBetween(0,255) + "," + randomBetween(0,255) + "," + randomBetween(0,255) + ")" ;
+//Set background color of div to be new rgb
+const setNewColor = () => {
+    const rgb = "rgb(" + randomBetween(0,255) + "," + randomBetween(0,255) + "," + randomBetween(0,255) + ")" ;
+    console.log(rgb)
+}
+
+
+
+
