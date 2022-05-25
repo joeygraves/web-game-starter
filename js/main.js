51baf5ea3336c04fab89dd43cd1e00e1987c4a90
Joseph Graves <jcg97@Josephs-MacBook-Pro.local>
2022-05-25 18:20:25 -0400

    created a win function and switched out the if statement

--- js/main.js
+++ js/main.js
@@ -1,16 +1,48 @@
-
-
 const randomBetween = (min, max) => {
-    min + Math.floor(Math.random() * (max - min + 1));
+    return min + Math.floor(Math.random() * (max - min + 1));
 }
 
-//Set background color of div to be new rgb
 const setNewColor = () => {
     const rgb = "rgb(" + randomBetween(0,255) + "," + randomBetween(0,255) + "," + randomBetween(0,255) + ")" ;
     console.log(rgb)
+    return rgb
 }
 
-document.querySelector("button").addEventListener("boxes", setNewColor);
+const answer = setNewColor();
+document.querySelector("#rgbValue").textContent = answer;
+
+const options = Array.from(document.querySelectorAll(".color-option"));
+console.log(options);
+
+const answerIndex = randomBetween(0, 2);
+
+for (let i = 0; i < options.length; i++){
+    if (i === answerIndex){
+        options[i].style.backgroundColor = answer ;
+        options[i].addEventListener('click', win);
+
+    }
+
+    else {
+        options[i].style.backgroundColor = setNewColor();
+        options[i].addEventListener('click', setNewColor);
+    }
+}
+//change to win and lose functions
+//const guess = () => {
+   //win function if(document.activeElement.id == index){
+        //document.getElementById("#rgbValue").innerHTML = "Cozy Vibess, Goodjob!!";
+        //document.getElementById("#rgbValue").style.color="light purple";
+    }
+   //lose function else {
+        document.getElementById("#rgbValue").innerHTML = "Go back to CLASS!!";
+        document.getElementById("#rgbValue").style.color="red";
+    }
+}
 
 
+const win = () => {
+    document.getElementById("#rgbValue").innerHTML = "Cozy Vibess, Goodjob!!";
+    document.getElementById("#rgbValue").style.color="light purple";
+}
 
